747b6e2ed70d7da2fad937e11ac1b9833750458a
Amir Rajan <ar@amirrajan.net>
2023-12-03 14:07:16 -0600

    disable lights

--- keyboards/keebio/nyquist/keymaps/amirrajan/keymap.c
+++ keyboards/keebio/nyquist/keymaps/amirrajan/keymap.c
@@ -24,13 +24,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  KC_TAB,        KC_Q,           KC_W,             KC_E,           KC_R,           KC_T,                   KC_Y,            KC_U,           KC_I,            KC_O,           KC_P,            GUI_T(KC_BACKSLASH), \
  KC_LCTL,       KC_A,           KC_S,             KC_D,           KC_F,           KC_G,                   KC_H,            KC_J,           KC_K,            KC_L,           KC_SEMICOLON,    RCTL_T(KC_QUOTE),    \
  KC_LSFT,       KC_Z,           KC_X,             KC_C,           KC_V,           KC_B,                   KC_N,            KC_M,           KC_COMMA,        KC_DOT,         KC_SLASH,        KC_RSFT,             \
- KC_LCTL,       KC_LALT,        KC_LCMD,          KC_EQUAL,       KC_UNDS,        LT(1, KC_ENTER),        LT(2, KC_SPACE), KC_BSPC,        KC_LEFT,         KC_DOWN,        KC_UP,           KC_RGHT \
+ KC_LCTL,       KC_LALT,        KC_LCMD,          KC_EQUAL,       KC_UNDS,        LT(1, KC_ENTER),        KC_SPACE,        KC_BSPC,        KC_LEFT,         KC_DOWN,        KC_UP,           KC_RGHT \
 ),
 
 [_SYMBOLS] = LAYOUT_ortho_5x12(
  KC_TILDE,      KC_EXCLAIM,     KC_AT,            KC_HASH,        KC_DOLLAR,      KC_PERCENT,            KC_CIRCUMFLEX,    KC_AMPERSAND,   KC_ASTERISK,     KC_LEFT_PAREN,  KC_RIGHT_PAREN,  KC_BACKSPACE,        \
  KC_TAB,        KC_QUOTE,       KC_EQUAL,         KC_EQUAL,       KC_ENTER,       KC_TILDE,              KC_QUESTION,      KC_LEFT_PAREN,  KC_RIGHT_PAREN,  KC_PIPE,        KC_PLUS,         KC_BACKSLASH,        \
- KC_LEFT,       KC_AMPERSAND,   KC_AT,            KC_MINUS,       KC_UNDS,        KC_RABK,               KC_HASH,          KC_LBRC,        KC_RBRC,         KC_LABK,        KC_COLON,        KC_DQUO,             \
+ KC_LEFT,       KC_AMPERSAND,   S(KC_S),          KC_MINUS,       KC_UNDS,        KC_RABK,               KC_HASH,          KC_LBRC,        KC_RBRC,         KC_LABK,        KC_COLON,        KC_DQUO, \
  KC_RGHT,       KC_COMMA,       KC_ASTERISK,      KC_COMMA,       LGUI(KC_V),     KC_BACKSPACE,          KC_EXCLAIM,       KC_LCBR,        KC_RCBR,         KC_RABK,        KC_QUESTION,     KC_0,                \
  KC_DOWN,       KC_LALT,        KC_LCMD,          KC_DOLLAR,      KC_MS_BTN1,     KC_SPACE,              KC_SPACE,         KC_BACKSPACE,   KC_LEFT,         KC_DOWN,        KC_UP,           KC_RGHT              \
 ),
@@ -61,3 +61,8 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_MODS] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) }
 };
 #endif
+
+void keyboard_post_init_user(void) {
+    // Call the post init code.
+    rgblight_disable_noeeprom();  // Turn off RGB lighting.
+}
