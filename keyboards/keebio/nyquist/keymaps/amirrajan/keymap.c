a383c0c18890b18116b65a6ae7a758fa3f2ab81a
Amir Rajan <ar@amirrajan.net>
2024-04-12 18:30:52 -0500

    single quote mapped to meta

--- keyboards/keebio/nyquist/keymaps/amirrajan/keymap.c
+++ keyboards/keebio/nyquist/keymaps/amirrajan/keymap.c
@@ -3,7 +3,7 @@
 
 #include QMK_KEYBOARD_H
 
-#define MOUSEKEY_DELAY             0
+#define MOUSEKEY_DELAY             500
 #define MOUSEKEY_INTERVAL          20
 #define MOUSEKEY_MAX_SPEED         5
 #define MOUSEKEY_TIME_TO_MAX       50
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ortho_5x12(
  KC_GRAVE,      KC_1,           KC_2,             KC_3,           KC_4,           KC_5,                   KC_6,            KC_7,           KC_8,            KC_9,           KC_0,            KC_MINUS,            \
  KC_TAB,        KC_Q,           KC_W,             KC_E,           KC_R,           KC_T,                   KC_Y,            KC_U,           KC_I,            KC_O,           KC_P,            GUI_T(KC_BACKSLASH), \
- KC_LCTL,       KC_A,           KC_S,             KC_D,           KC_F,           KC_G,                   KC_H,            KC_J,           KC_K,            KC_L,           KC_SEMICOLON,    RCTL_T(KC_QUOTE),    \
+ KC_LCTL,       KC_A,           KC_S,             KC_D,           KC_F,           KC_G,                   KC_H,            KC_J,           KC_K,            KC_L,           KC_SEMICOLON,    LALT_T(KC_QUOTE),    \
  KC_LSFT,       KC_Z,           KC_X,             KC_C,           KC_V,           KC_B,                   KC_N,            KC_M,           KC_COMMA,        KC_DOT,         KC_SLASH,        KC_RSFT,             \
  KC_LCTL,       KC_LALT,        KC_LCMD,          KC_EQUAL,       KC_UNDS,        LT(1, KC_ENTER),        KC_SPACE,        KC_BSPC,        KC_LEFT,         KC_DOWN,        KC_UP,           KC_RGHT \
 ),
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  KC_TILDE,      KC_EXCLAIM,     KC_AT,            KC_HASH,        KC_DOLLAR,      KC_PERCENT,            KC_CIRCUMFLEX,    KC_AMPERSAND,   KC_ASTERISK,     KC_LEFT_PAREN,  KC_RIGHT_PAREN,  KC_BACKSPACE,        \
  KC_TAB,        KC_QUOTE,       KC_EQUAL,         KC_EQUAL,       KC_ENTER,       KC_TILDE,              KC_QUESTION,      KC_LEFT_PAREN,  KC_RIGHT_PAREN,  KC_PIPE,        KC_PLUS,         KC_BACKSLASH,        \
  KC_LEFT,       KC_AMPERSAND,   S(KC_S),          KC_MINUS,       KC_UNDS,        KC_RABK,               KC_HASH,          KC_LBRC,        KC_RBRC,         KC_LABK,        KC_COLON,        KC_DQUO,             \
- KC_RGHT,       KC_COMMA,       KC_ASTERISK,      KC_COMMA,       LGUI(KC_V),     KC_BACKSPACE,          KC_EXCLAIM,       KC_LCBR,        KC_RCBR,         KC_RABK,        KC_QUESTION,     KC_0,                \
- KC_DOWN,       KC_LALT,        KC_LCMD,          KC_DOLLAR,      KC_MS_BTN1,     KC_SPACE,              KC_BTN1,          KC_BTN2,        KC_MS_L,         KC_MS_D,        KC_MS_U,         KC_MS_R              \
+ KC_RGHT,       KC_COMMA,       KC_ASTERISK,      KC_COMMA,       LGUI(KC_V),     KC_BACKSPACE,          KC_EXCLAIM,       KC_LCBR,        KC_RCBR,         KC_RABK,        KC_MS_U,         KC_0,                \
+ KC_DOWN,       KC_LALT,        KC_LCMD,          KC_MS_BTN2,     KC_MS_BTN1,     KC_SPACE,              KC_SPACE,         KC_BSPC,        KC_MS_BTN1,      KC_MS_L,        KC_MS_D,         KC_MS_R              \
 ),
 
 [_MODS] = LAYOUT_ortho_5x12(
