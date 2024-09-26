5727ba9b3d4abb40f221e887d0d700d6b1dbd795
Amir Rajan <ar@amirrajan.net>
2024-09-25 23:32:21 -0500

    steno keyboard wip.

--- keyboards/keebio/nyquist/keymaps/amirrajan/keymap.c
+++ keyboards/keebio/nyquist/keymaps/amirrajan/keymap.c
@@ -15,13 +15,16 @@
 enum layer_names {
     _QWERTY,
     _SYMBOLS,
-    _MODS
+    _MODS,
+    _PLOVER
 };
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   PHOTOSHOP_PREV_LAYER,
-  PHOTOSHOP_NEXT_LAYER
+  PHOTOSHOP_NEXT_LAYER,
+  PLOVER_OR_0,
+  QWERTY_OR_0,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -42,18 +45,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             // when keycode QMKBEST is released
         }
         break;
+    case PLOVER_OR_0:
+        if (record->event.pressed) {
+            if (layer_state_is(_PLOVER)) {
+                layer_off(_PLOVER);
+                layer_on(_QWERTY);
+            } else {
+                // if control is move to layer 3 and do not send 0
+                if (get_mods() & MOD_BIT(KC_LCTL)) {
+                    layer_on(_PLOVER);
+                    return false;
+                } else {
+                    SEND_STRING(SS_TAP(X_0));
+                }
+            }
+        }
+        break;
     }
+
     return true;
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_ortho_5x12(
- KC_GRAVE,      KC_1,           KC_2,             KC_3,           KC_4,           KC_5,                   KC_6,            KC_7,           KC_8,            KC_9,           KC_0,            KC_MINUS,            \
+ KC_GRAVE,      KC_1,           KC_2,             KC_3,           KC_4,           KC_5,                   KC_6,            KC_7,           KC_8,            KC_9,           PLOVER_OR_0,     KC_MINUS,            \
  KC_TAB,        KC_Q,           KC_W,             KC_E,           KC_R,           KC_T,                   KC_Y,            KC_U,           KC_I,            KC_O,           KC_P,            GUI_T(KC_BACKSLASH), \
  KC_LCTL,       KC_A,           KC_S,             KC_D,           KC_F,           KC_G,                   KC_H,            KC_J,           KC_K,            KC_L,           KC_SEMICOLON,    LALT_T(KC_QUOTE),    \
  KC_LSFT,       KC_Z,           KC_X,             KC_C,           KC_V,           KC_B,                   KC_N,            KC_M,           KC_COMMA,        KC_DOT,         KC_SLASH,        KC_RSFT,             \
- KC_LCTL,       KC_LALT,        KC_LCMD,          KC_EQUAL,       KC_UNDS,        LT(1, KC_ENTER),        KC_SPACE,        KC_BSPC,        KC_LEFT,         KC_DOWN,        KC_UP,           KC_RGHT              \
+ KC_LCTL,       KC_LALT,        KC_LCMD,          KC_EQUAL,       KC_UNDS,        LT(1, KC_ENTER),        KC_SPACE,        KC_BSPC,        KC_LEFT,         KC_DOWN,        KC_UP,           KC_RGHT \
 ),
 
 [_SYMBOLS] = LAYOUT_ortho_5x12(
@@ -70,6 +90,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  XXXXXXX,       XXXXXXX,        XXXXXXX,          XXXXXXX,        XXXXXXX,        XXXXXXX,               XXXXXXX,          XXXXXXX,        XXXXXXX,         XXXXXXX,        XXXXXXX,         XXXXXXX,        \
  XXXXXXX,       XXXXXXX,        XXXXXXX,          XXXXXXX,        LGUI(KC_V),     XXXXXXX,               XXXXXXX,          XXXXXXX,        XXXXXXX,         XXXXXXX,        XXXXXXX,         XXXXXXX,        \
  XXXXXXX,       XXXXXXX,        XXXXXXX,          XXXXXXX,        XXXXXXX,        XXXXXXX,               XXXXXXX,          XXXXXXX,        XXXXXXX,         XXXXXXX,        XXXXXXX,         XXXXXXX         \
+),
+
+[_PLOVER] = LAYOUT_ortho_5x12(
+ KC_TILDE,      KC_EXCLAIM,     KC_AT,            KC_HASH,        KC_DOLLAR,      KC_PERCENT,            KC_CIRCUMFLEX,    KC_AMPERSAND,   KC_ASTERISK,     KC_LEFT_PAREN,  PLOVER_OR_0,       KC_BACKSPACE,         \
+ KC_TAB,        KC_QUOTE,       KC_EQUAL,         KC_EQUAL,       KC_ENTER,       KC_TILDE,              KC_QUESTION,      KC_LEFT_PAREN,  KC_RIGHT_PAREN,  KC_PIPE,        KC_PLUS,              GUI_T(KC_BACKSLASH),  \
+ KC_LEFT,       KC_AMPERSAND,   S(KC_S),          KC_MINUS,       KC_UNDS,        KC_RABK,               KC_HASH,          KC_LBRC,        KC_RBRC,         KC_LABK,        KC_COLON,             KC_DQUO,              \
+ KC_RGHT,       KC_COMMA,       KC_ASTERISK,      KC_MS_BTN1,     LGUI(KC_V),     KC_BACKSPACE,          KC_EXCLAIM,       KC_LCBR,        KC_RCBR,         KC_RABK,        PHOTOSHOP_PREV_LAYER, PHOTOSHOP_NEXT_LAYER, \
+ KC_DOWN,       KC_LALT,        KC_LCMD,          KC_MS_BTN2,     KC_MS_BTN1,     KC_SPACE,              KC_SPACE,         KC_BSPC,        KC_MS_L,         KC_MS_D,        KC_MS_U,              KC_MS_R               \
 )
 
 };
@@ -87,7 +115,8 @@ void persistent_default_layer_set(uint16_t default_layer) {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_QWERTY] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
     [_SYMBOLS] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
-    [_MODS] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) }
+    [_MODS] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [_PLOVER] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) }
 };
 #endif
 
