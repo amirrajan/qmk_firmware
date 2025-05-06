92580a854614b730b1013d96d220aea16f42439a
Amir Rajan <ar@amirrajan.net>
2025-05-06 17:14:18 -0500

    swapped location of option and command

--- keyboards/sofle/keymaps/amirrajan/keymap.c
+++ keyboards/sofle/keymaps/amirrajan/keymap.c
@@ -1,6 +1,8 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
+#undef TAPPING_TERM
+#define TAPPING_TERM 100
 
 enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
@@ -33,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F1,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  LGUI_T(KC_BACKSLASH),
-  KC_LCTL,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  MT(MOD_LALT, KC_QUOTE),
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, QK_BOOT,    XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-            KC_LCTL,KC_LALT, KC_UNDS, LT(1, KC_ENTER), KC_UNDS,          KC_UP,  KC_SPC, KC_BSPC, KC_G, KC_G
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,   KC_0,     KC_F1,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     MT(MOD_LALT, KC_BACKSLASH),
+  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  LGUI_T(KC_QUOTE),
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,            QK_BOOT,   XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
+                    KC_LCTL, KC_LALT, KC_UNDS, LT(1, KC_ENTER), KC_DOWN,   KC_UP,   KC_SPC,  KC_BSPC, KC_LEFT, KC_RIGHT
 ),
 
 [_SYMBOLS] = LAYOUT(
