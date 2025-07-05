bc35bbd9f0faf854c8b9ed1b103e93e706ae2801
Amir Rajan <ar@amirrajan.net>
2025-07-04 20:20:18 -0500

    swapped up and down on thumb cluster

--- keyboards/sofle/keymaps/amirrajan/keymap.c
+++ keyboards/sofle/keymaps/amirrajan/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     MT(MOD_LALT, KC_BACKSLASH),
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  LGUI_T(KC_QUOTE),
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,            QK_BOOT,   KC_PEQL, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
-                    KC_LCTL, KC_LALT, KC_UNDS, LT(1, KC_ENTER), KC_DOWN,   KC_UP, LT(1, KC_SPC),  KC_BSPC, KC_LEFT, KC_RIGHT
+                    KC_LCTL, KC_LALT, KC_UNDS, LT(1, KC_ENTER), KC_UP,     KC_DOWN, LT(1, KC_SPC),  KC_BSPC, KC_LEFT, KC_RIGHT
 ),
 
 [_SYMBOLS] = LAYOUT(
